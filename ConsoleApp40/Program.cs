c362b38cb447c7f9cc2c21b4d8b43aad0fc5f612
saikumarreddy01919 <asaireddy1919@gmail.com>
2021-11-01 09:47:37 +0530

    change invs

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp40
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("from vs");
             Console.ReadLine();
         }
     }
